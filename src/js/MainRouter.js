44e3abc1c7afbd1fcdd71bca196c6dce1c3abc36
jhamPac <cloud@kevia.me>
2019-07-06 16:28:18 -0700

    prettier did its thing

--- src/js/MainRouter.js
+++ src/js/MainRouter.js
@@ -1,17 +1,17 @@
 import React from 'react'
-import { HashRouter as Router, Route, Switch } from 'react-router-dom'
+import {HashRouter as Router, Route, Switch} from 'react-router-dom'
 
-const LandingPage = props => (<div>PHX 2084</div>)
+const LandingPage = props => <div>PHX 2084</div>
 
 export default function MainRouter() {
   return (
     <Router>
       <main id="UI" className="container">
-          <Switch>
-            <Route exact path="/">
-              <LandingPage />
-            </Route>
-          </Switch>
+        <Switch>
+          <Route exact path="/">
+            <LandingPage />
+          </Route>
+        </Switch>
       </main>
     </Router>
   )
--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,12 @@
-import React    from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom'
 
 import MainRouter from './MainRouter'
 
 const render = Component => {
-  ReactDOM.render(
-    <Component />,
-    document.getElementById('mount-point')
-  )
+  ReactDOM.render(<Component />, document.getElementById('mount-point'))
 }
 
-(document.readyState === 'loading')
+document.readyState === 'loading'
   ? document.addEventListener('DOMContentLoaded', () => render(MainRouter))
   : render(Application)
