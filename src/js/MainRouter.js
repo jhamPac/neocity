83287b3ee119ddc496fb27750ea487b83622602e
jhamPac <cloud@kevia.me>
2019-07-06 16:05:37 -0700

    basic project setup

---
+++ src/js/MainRouter.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { HashRouter as Router, Route, Switch } from 'react-router-dom'
+
+const LandingPage = props => (<div>PHX 2084</div>)
+
+export default function MainRouter() {
+  return (
+    <Router>
+      <main id="UI" className="container">
+          <Switch>
+            <Route exact path="/">
+              <LandingPage />
+            </Route>
+          </Switch>
+        <Footer />
+      </main>
+    </Router>
+  )
+}
---
+++ src/js/index.js
@@ -0,0 +1,15 @@
+import React    from 'react'
+import ReactDOM from 'react-dom'
+
+import MainRouter from './MainRouter'
+
+const render = Component => {
+  ReactDOM.render(
+    <Component />,
+    document.getElementById('mount-point')
+  )
+}
+
+(document.readyState === 'loading')
+  ? document.addEventListener('DOMContentLoaded', () => render(MainRouter))
+  : render(Application)
