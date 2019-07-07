8371b352640744056678b7bb9db4a461a3ee89f3
jhamPac <cloud@kevia.me>
2019-07-07 06:57:34 -0700

    using a rc file prettier instead json files

--- src/js/LandingPage.js
+++ src/js/LandingPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react'
 
 export default function LandingPage() {
   return (
@@ -21,5 +21,5 @@ export default function LandingPage() {
         <li>no connections for upward mobility</li>
       </ul>
     </div>
-  );
+  )
 }
--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-import MainRouter from "./MainRouter";
+import MainRouter from './MainRouter'
 
 const render = Component => {
-  ReactDOM.render(<Component />, document.getElementById("mount-point"));
-};
+  ReactDOM.render(<Component />, document.getElementById('mount-point'))
+}
 
-document.readyState === "loading"
-  ? document.addEventListener("DOMContentLoaded", () => render(MainRouter))
-  : render(Application);
+document.readyState === 'loading'
+  ? document.addEventListener('DOMContentLoaded', () => render(MainRouter))
+  : render(Application)
