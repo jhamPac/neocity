332a21c06d30052a7477966eb0b8545373d16479
kevia <kevia.cloud@endurance.com>
2021-02-21 12:23:07 -0700

    refactor: add theme for react95

--- src/js/MainRouter.js
+++ src/js/MainRouter.js
@@ -1,20 +1,47 @@
-import React from 'react'
+import * as React from 'react'
 import {HashRouter as Router, Route, Switch} from 'react-router-dom'
-import injectSheet from 'react-jss'
+import {createGlobalStyle, ThemeProvider} from 'styled-components'
+import {styleReset} from 'react95'
+
+import original from 'react95/dist/themes/original'
+import ms_sans_serif from 'react95/dist/fonts/ms_sans_serif.woff2'
+import ms_sans_serif_bold from 'react95/dist/fonts/ms_sans_serif_bold.woff2'
 
 import LandingPage from './LandingPage'
-import styles from './styles'
 
-const MainRouter = ({classes}) => (
-  <Router>
-    <main className={classes.ui}>
-      <Switch>
-        <Route exact path="/">
-          <LandingPage />
-        </Route>
-      </Switch>
-    </main>
-  </Router>
-)
+const GlobalStyles = createGlobalStyle`
+  @font-face {
+    font-family: 'ms_sans_serif';
+    src: url('${ms_sans_serif}') format('woff2');
+    font-weight: 400;
+    font-style: normal
+  }
+  @font-face {
+    font-family: 'ms_sans_serif';
+    src: url('${ms_sans_serif_bold}') format('woff2');
+    font-weight: bold;
+    font-style: normal
+  }
+  body {
+    background: #008080;
+    font-family: 'ms_sans_serif';
+  }
+  ${styleReset}
+`
 
-export default injectSheet(styles)(MainRouter)
+export default function MainRouter() {
+  return (
+    <Router>
+      <GlobalStyles />
+      <main>
+        <Switch>
+          <Route exact path="/">
+            <ThemeProvider theme={original}>
+              <LandingPage />
+            </ThemeProvider>
+          </Route>
+        </Switch>
+      </main>
+    </Router>
+  )
+}
